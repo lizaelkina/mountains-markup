f6ace2dd5cc19345cbd89f0d65759be42394f753
Liza Elkina <lizapetkova@yandex.ru>
2024-10-25 23:45:51 +0300

    future: active tab when scrolling down to section

--- src/blocks/components/slider/slider.js
+++ src/blocks/components/slider/slider.js
@@ -1,12 +1,38 @@
 const page = document.querySelector('.page');
 const slider = page.querySelector('.slider');
-const sliderTabs = slider.querySelectorAll('.slider__tab');
+const tabList = slider.querySelectorAll('.js-tab');
+const tabContentList = page.querySelectorAll('.js-tab-content');
 
-sliderTabs.forEach((tab) => {
-  tab.addEventListener('click', (event) => {
-    sliderTabs.forEach((t) => {
-      t.classList.remove('slider__tab_active');
-    });
-    event.currentTarget.classList.add('slider__tab_active');
+window.addEventListener('scroll', () => {
+  const scrollTop = window.scrollY;
+  const scrollBottom = scrollTop + window.innerHeight;
+  const scrollCenter = scrollTop + window.innerHeight / 2;
+  let selectedTab = 0;
+  const tabInViewport = [];
+  tabContentList.forEach((tabContent, index) => {
+    let bottomOfTabContent = tabContent.offsetTop + tabContent.offsetHeight;
+    if (bottomOfTabContent > scrollTop && bottomOfTabContent < scrollBottom ||
+      tabContent.offsetTop > scrollTop && tabContent.offsetTop < scrollBottom) {
+      tabInViewport.push(index);
+    }
+  });
+
+  let minDistance = Number.MAX_VALUE;
+  tabInViewport.forEach((index) => {
+    const tabContent = tabContentList[index];
+    const titleCenter = tabContent.offsetTop + tabContent.offsetHeight / 2;
+    let distance = Math.abs(scrollCenter - titleCenter);
+    if (distance < minDistance) {
+      selectedTab = index;
+      minDistance = distance;
+    }
+  });
+
+  tabList.forEach((tab, index) => {
+    if (index === selectedTab) {
+      tab.classList.add('slider__tab_active');
+    } else {
+      tab.classList.remove('slider__tab_active');
+    }
   });
 });
