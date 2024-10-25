192cbdaa638fc1821030b5291125639fea724034
Liza Elkina <lizapetkova@yandex.ru>
2024-10-25 17:23:15 +0300

    future: add class active on click

--- src/blocks/components/slider/slider.js
+++ src/blocks/components/slider/slider.js
@@ -1,1 +1,12 @@
+const page = document.querySelector('.page');
+const slider = page.querySelector('.slider');
+const sliderTabs = slider.querySelectorAll('.slider__tab');
 
+sliderTabs.forEach((tab) => {
+  tab.addEventListener('click', (event) => {
+    sliderTabs.forEach((t) => {
+      t.classList.remove('slider__tab_active');
+    });
+    event.currentTarget.classList.add('slider__tab_active');
+  });
+});
